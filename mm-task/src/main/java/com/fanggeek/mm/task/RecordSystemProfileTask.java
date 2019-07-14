2d5545d34dca17f3d39101194133ea90981a1f68
huangwei <huangwei@fanggeek.com>
2019-07-14 14:35:00 +0800

    打印日志，测试一下

--- mm-task/src/main/java/com/fanggeek/mm/task/RecordSystemProfileTask.java
+++ mm-task/src/main/java/com/fanggeek/mm/task/RecordSystemProfileTask.java
@@ -1,15 +1,23 @@
 package com.fanggeek.mm.task;
 
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import com.ctrip.framework.apollo.Config;
 import com.ctrip.framework.apollo.spring.annotation.ApolloConfig;
+import com.fanggeek.mm.dao.model.doc.SystemProfileDocument;
 import com.fanggeek.mm.db.dao.ProfileDAO;
+import com.mongodb.BasicDBObject;
 
 @Component
 public class RecordSystemProfileTask {
+    
+    private static final Logger LOGGER = LoggerFactory.getLogger(RecordSystemProfileTask.class);
 
     @Autowired
     private ProfileDAO profileDAO;
@@ -20,7 +28,13 @@ public class RecordSystemProfileTask {
     @Scheduled(cron = "0 0/1 * * * ?")
     public void recordTask() {
         
-//        profileDAO.
+        LOGGER.info("RecordSystemProfileTask recordTask");
+        
+        List<BasicDBObject> listByOid = profileDAO.getListByOid(null, 1);
+        
+        LOGGER.info("result is {}", listByOid);
+        
+//        List<SystemProfileDocument> listByOid = profileDAO.getListByOid(null, 20);
         
 //        config = ApolloUtils.getConfig();
     }
