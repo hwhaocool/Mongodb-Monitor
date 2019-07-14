48f5279832b9a142fa1496a44a1047fafad2230c
huangwei <huangwei@fanggeek.com>
2019-07-14 21:25:35 +0800

    改包名

--- mm-web/src/main/java/com/fanggeek/mm/web/controller/SlowController.java
+++ mm-web/src/main/java/com/fanggeek/mm/web/controller/SlowController.java
@@ -1,12 +1,14 @@
-package com.fanggeek.msg.web.controller;
+package com.fanggeek.mm.web.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fanggeek.mm.model.vo.SlowList;
+import com.fanggeek.mm.service.SlowOpService;
 
 
 /**
@@ -19,13 +21,24 @@ import com.fanggeek.mm.model.vo.SlowList;
 @RestController
 public class SlowController {
     
+    @Autowired
+    private SlowOpService slowService;
+    
     //健康检查接口
+    @RequestMapping(value = "/query", method = RequestMethod.GET)
+    public SlowList info2() {
+        slowService.test();
+        
+        return new SlowList();
+    }
+    
     @RequestMapping(value = "/query", method = RequestMethod.POST)
     public SlowList info(
             HttpServletRequest request) {
         
         SlowList list = new SlowList();
-
+        
+        slowService.test();
 
         return list;
     }
