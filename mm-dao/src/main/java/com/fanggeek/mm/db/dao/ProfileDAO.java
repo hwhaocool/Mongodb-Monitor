ec45034a7634ab47d5b54cbf19f2a52c17022b84
huangwei <huangwei@fanggeek.com>
2019-07-14 14:35:12 +0800

    改为泛型 T 类型

--- mm-dao/src/main/java/com/fanggeek/mm/db/dao/ProfileDAO.java
+++ mm-dao/src/main/java/com/fanggeek/mm/db/dao/ProfileDAO.java
@@ -1,10 +1,16 @@
 package com.fanggeek.mm.db.dao;
 
+import java.util.List;
+
+import org.jongo.Find;
+import org.jongo.MongoCursor;
+
 import com.fanggeek.mm.common.constants.Constants;
 import com.fanggeek.mm.dao.model.doc.SystemProfileDocument;
 import com.fanggeek.mm.db.MongoDAOSupport;
+import com.mongodb.BasicDBObject;
 
-public class ProfileDAO extends MongoDAOSupport<SystemProfileDocument> {
+public class ProfileDAO extends MongoDAOSupport<BasicDBObject> {
 
     @Override
     protected String collectionName() {
@@ -15,5 +21,20 @@ public class ProfileDAO extends MongoDAOSupport<SystemProfileDocument> {
     public void createIndexs() {
         
     }
+    
+//    @Override
+//    protected List<T> getList(String query, String sort, int limit) {
+//        
+//        Find find = getCollection().find(query).sort(sort).skip(0).limit(limit);
+//        
+//        
+//        MongoCursor<T> xxx= (MongoCursor<T>) find.as(BasicDBObject.class);
+//        
+//        
+//        List<T> list = toList(xxx);
+//        
+//        
+//        return list;
+//    }
 
 }
