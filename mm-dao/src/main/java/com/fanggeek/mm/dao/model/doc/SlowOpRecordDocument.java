19b8f4dd846e1f5f7427487beee8434d6c14b0b2
huangwei <huangwei@fanggeek.com>
2019-07-15 18:55:51 +0800

    新增 document

--- mm-dao/src/main/java/com/fanggeek/mm/dao/model/doc/SlowOpRecordDocument.java
+++ mm-dao/src/main/java/com/fanggeek/mm/dao/model/doc/SlowOpRecordDocument.java
@@ -1,5 +1,347 @@
 package com.fanggeek.mm.dao.model.doc;
 
-public class SlowOpRecordDocument {
+import java.util.Date;
 
+public class SlowOpRecordDocument {
+    
+    private String _id;                     //
+    private Date ts;                        //发生时间
+
+    private String op;                      //操作类型，query
+    private String db;                      //库名，fanggeek
+    private String ns;                      //表名，fanggeek.odin_checkins
+    
+    private Object query;                   //查询语句,op=query
+    private Object command;                 //查询语句, op=command
+    
+    private Integer keysExamined;           //索引扫描数量
+    private Integer docsExamined;           //文档扫描数量
+    
+    private Boolean cursorExhausted;        //游标
+    
+    private Integer numYield;               //该操作为了使其他操作完成而放弃的次数
+    
+    private Object locks;                   //锁信息
+    
+    private Integer nreturned;              //返回的文档数量
+    private Integer responseLength;         //返回字节长度，如果这个数字很大，考虑值返回所需字段
+    
+    private String protocol;                //协议
+    
+    private Integer millis;                 //耗时
+    
+    private String planSummary;             //操作计划描述，IXSCAN #索引扫描，COLLSCAN #全表扫描，#根据索引去检索指定document
+    private Object execStats;               //执行状态
+    
+    private String client;                  //客户端ip
+    private String user;                    //帐号信息，fanggeek_frontend_trident
+    
+    
+    
+    /**
+     * <br> ObjectId
+     *
+     * @return
+     * @author YellowTail
+     * @since 2019-07-15
+     */
+    public String get_id() {
+        return _id;
+    }
+
+    public void set_id(String _id) {
+        this._id = _id;
+    }
+
+    /**
+     * <br>操作类型，query
+     *
+     * @return
+     * @author YellowTail
+     * @since 2019-07-15
+     */
+    public String getOp() {
+        return op;
+    }
+    
+    public void setOp(String op) {
+        this.op = op;
+    }
+    
+    /**
+     * <br>库名，fanggeek
+     *
+     * @return
+     * @author YellowTail
+     * @since 2019-07-15
+     */
+    public String getDb() {
+        return db;
+    }
+    
+    public void setDb(String db) {
+        this.db = db;
+    }
+    
+    /**
+     * <br>表名，odin_checkins
+     *
+     * @return
+     * @author YellowTail
+     * @since 2019-07-15
+     */
+    public String getNs() {
+        return ns;
+    }
+    
+    public void setNs(String ns) {
+        this.ns = ns;
+    }
+    
+    /**
+     * <br>查询语句
+     *
+     * @return
+     * @author YellowTail
+     * @since 2019-07-15
+     */
+    public Object getQuery() {
+        return query;
+    }
+    public void setQuery(Object query) {
+        this.query = query;
+    }
+    
+    /**
+     * <br>查询语句
+     *
+     * @return
+     * @author YellowTail
+     * @since 2019-07-15
+     */
+    public Object getCommand() {
+        return command;
+    }
+    
+    public void setCommand(Object command) {
+        this.command = command;
+    }
+    
+    /**
+     * <br>索引扫描数量
+     *
+     * @return
+     * @author YellowTail
+     * @since 2019-07-15
+     */
+    public Integer getKeysExamined() {
+        return keysExamined;
+    }
+    
+    public void setKeysExamined(Integer keysExamined) {
+        this.keysExamined = keysExamined;
+    }
+    
+    /**
+     * <br>文档扫描数量
+     *
+     * @return
+     * @author YellowTail
+     * @since 2019-07-15
+     */
+    public Integer getDocsExamined() {
+        return docsExamined;
+    }
+    
+    public void setDocsExamined(Integer docsExamined) {
+        this.docsExamined = docsExamined;
+    }
+    
+    public Boolean getCursorExhausted() {
+        return cursorExhausted;
+    }
+    
+    public void setCursorExhausted(Boolean cursorExhausted) {
+        this.cursorExhausted = cursorExhausted;
+    }
+    
+    /**
+     * <br>该操作为了使其他操作完成而放弃的次数
+     *
+     * @return
+     * @author YellowTail
+     * @since 2019-07-15
+     */
+    public Integer getNumYield() {
+        return numYield;
+    }
+    
+    public void setNumYield(Integer numYield) {
+        this.numYield = numYield;
+    }
+    
+    /**
+     * <br>锁信息
+     *
+     * @return
+     * @author YellowTail
+     * @since 2019-07-15
+     */
+    public Object getLocks() {
+        return locks;
+    }
+    
+    public void setLocks(Object locks) {
+        this.locks = locks;
+    }
+    
+    /**
+     * <br>返回的文档数量
+     *
+     * @return
+     * @author YellowTail
+     * @since 2019-07-15
+     */
+    public Integer getNreturned() {
+        return nreturned;
+    }
+    
+    public void setNreturned(Integer nreturned) {
+        this.nreturned = nreturned;
+    }
+    
+    /**
+     * <br>返回字节长度，如果这个数字很大，考虑值返回所需字段
+     *
+     * @return
+     * @author YellowTail
+     * @since 2019-07-15
+     */
+    public Integer getResponseLength() {
+        return responseLength;
+    }
+    
+    public void setResponseLength(Integer responseLength) {
+        this.responseLength = responseLength;
+    }
+    
+    /**
+     * <br>协议
+     *
+     * @return
+     * @author YellowTail
+     * @since 2019-07-15
+     */
+    public String getProtocol() {
+        return protocol;
+    }
+    
+    public void setProtocol(String protocol) {
+        this.protocol = protocol;
+    }
+    
+    /**
+     * <br>耗时, 单位 毫秒
+     *
+     * @return
+     * @author YellowTail
+     * @since 2019-07-15
+     */
+    public Integer getMillis() {
+        return millis;
+    }
+    
+    public void setMillis(Integer millis) {
+        this.millis = millis;
+    }
+    
+    /**
+     * <br>操作计划描述
+     * <br>IXSCAN #索引扫描，
+     * <br>COLLSCAN #全表扫描，
+     * <br>fetch #根据索引去检索指定document
+     *
+     * @return
+     * @author YellowTail
+     * @since 2019-07-15
+     */
+    public String getPlanSummary() {
+        return planSummary;
+    }
+    
+    public void setPlanSummary(String planSummary) {
+        this.planSummary = planSummary;
+    }
+    
+    /**
+     * <br>执行状态
+     *
+     * @return
+     * @author YellowTail
+     * @since 2019-07-15
+     */
+    public Object getExecStats() {
+        return execStats;
+    }
+    
+    public void setExecStats(Object execStats) {
+        this.execStats = execStats;
+    }
+    
+    /**
+     * <br>发生时间
+     *
+     * @return
+     * @author YellowTail
+     * @since 2019-07-15
+     */
+    public Date getTs() {
+        return ts;
+    }
+    
+    public void setTs(Date ts) {
+        this.ts = ts;
+    }
+    
+    /**
+     * <br>客户端ip
+     *
+     * @return
+     * @author YellowTail
+     * @since 2019-07-15
+     */
+    public String getClient() {
+        return client;
+    }
+    
+    public void setClient(String client) {
+        this.client = client;
+    }
+    
+    /**
+     * <br>帐号信息，fanggeek_frontend_trident
+     *
+     * @return
+     * @author YellowTail
+     * @since 2019-07-15
+     */
+    public String getUser() {
+        return user;
+    }
+    
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "SlowOpRecordDocument [_id=" + _id + ", ts=" + ts + ", op=" + op + ", db=" + db + ", ns=" + ns
+                + ", query=" + query + ", command=" + command + ", keysExamined=" + keysExamined + ", docsExamined="
+                + docsExamined + ", cursorExhausted=" + cursorExhausted + ", numYield=" + numYield + ", locks=" + locks
+                + ", nreturned=" + nreturned + ", responseLength=" + responseLength + ", protocol=" + protocol
+                + ", millis=" + millis + ", planSummary=" + planSummary + ", execStats=" + execStats + ", client="
+                + client + ", user=" + user + "]";
+    }
+    
+    
 }
