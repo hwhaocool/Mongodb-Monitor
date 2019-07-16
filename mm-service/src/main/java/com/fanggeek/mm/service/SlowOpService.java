f4d7b2eb3173471069bb939f103043109965235c
huangwei <huangwei@fanggeek.com>
2019-07-16 22:07:20 +0800

    提交 query 空方法

--- mm-service/src/main/java/com/fanggeek/mm/service/SlowOpService.java
+++ mm-service/src/main/java/com/fanggeek/mm/service/SlowOpService.java
@@ -1,14 +1,22 @@
 package com.fanggeek.mm.service;
 
-import java.util.List;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
+import org.apache.commons.codec.digest.DigestUtils;
+import org.bson.types.ObjectId;
+import org.joda.time.DateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.fanggeek.common.json.JSON2Helper;
 import com.fanggeek.mm.dao.model.doc.SlowOpRecordDocument;
 import com.fanggeek.mm.db.dao.ProfileDAO;
+import com.fanggeek.mm.db.dao.SlowOpRecordDAO;
 import com.mongodb.BasicDBObject;
 
 @Service
@@ -19,19 +27,11 @@ public class SlowOpService {
     @Autowired
     private ProfileDAO profileDAO;
     
-    public void test() {
-        LOGGER.info("RecordSystemProfileTask recordTask");
+    @Autowired
+    private SlowOpRecordDAO slowOpRecordDAO;
+    
+    public void query() {
         
-        List<BasicDBObject> list = profileDAO.getList(1);
-        
-//        List<BasicDBObject> listByOid = profileDAO.getListByOid(null, 1);
-        
-        LOGGER.info("result is {}", list);
     }
     
-    private SlowOpRecordDocument genDoc(BasicDBObject basicDBObject) {
-        SlowOpRecordDocument document = new SlowOpRecordDocument();
-        
-        return document;
-    }
 }
