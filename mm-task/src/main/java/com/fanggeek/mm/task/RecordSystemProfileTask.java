57e7c6a70e24e9f79f7a3758d8822204605a9532
huangwei <huangwei@fanggeek.com>
2019-07-16 21:37:07 +0800

    task 作为入口，调用一个服务

--- mm-task/src/main/java/com/fanggeek/mm/task/RecordSystemProfileTask.java
+++ mm-task/src/main/java/com/fanggeek/mm/task/RecordSystemProfileTask.java
@@ -1,18 +1,12 @@
 package com.fanggeek.mm.task;
 
-import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import com.ctrip.framework.apollo.Config;
-import com.ctrip.framework.apollo.spring.annotation.ApolloConfig;
-import com.fanggeek.mm.dao.model.doc.SystemProfileDocument;
-import com.fanggeek.mm.db.dao.ProfileDAO;
-import com.mongodb.BasicDBObject;
+import com.fanggeek.mm.service.Profile2SlowService;
 
 @Component
 public class RecordSystemProfileTask {
@@ -20,22 +14,15 @@ public class RecordSystemProfileTask {
     private static final Logger LOGGER = LoggerFactory.getLogger(RecordSystemProfileTask.class);
 
     @Autowired
-    private ProfileDAO profileDAO;
-    
-    @ApolloConfig
-    private Config config;
+    private Profile2SlowService profile2SlowService;
     
     @Scheduled(cron = "0 0/1 * * * ?")
     public void recordTask() {
         
-        LOGGER.info("RecordSystemProfileTask recordTask");
+        LOGGER.info("RecordSystemProfileTask recordTask start");
         
-        List<BasicDBObject> listByOid = profileDAO.getListByOid(null, 1);
+        profile2SlowService.recordAndSave2Other();
         
-        LOGGER.info("result is {}", listByOid);
-        
-//        List<SystemProfileDocument> listByOid = profileDAO.getListByOid(null, 20);
-        
-//        config = ApolloUtils.getConfig();
+        LOGGER.info("RecordSystemProfileTask recordTask end");
     }
 }
