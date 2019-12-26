5ee579f83e79295562536cfb7b5e0471bb831b71
huangwei <huangwei@fanggeek.com>
2019-12-26 16:02:27 +0800

    提交 监控数据库的配置

--- mm-dao/src/main/java/com/fanggeek/mm/dao/config/MonitorMajorJongoConfig.java
+++ mm-dao/src/main/java/com/fanggeek/mm/dao/config/MonitorMajorJongoConfig.java
@@ -1,43 +1,38 @@
 package com.fanggeek.mm.dao.config;
 
+import org.jongo.Jongo;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import com.fanggeek.mm.common.constants.Constants;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientOptions;
 import com.mongodb.MongoClientOptions.Builder;
 import com.mongodb.MongoClientURI;
-import org.jongo.Jongo;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConfigurationProperties(prefix = "mongodb")
-public class JongoConfig {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(JongoConfig.class);
+@ConfigurationProperties(prefix = "monitor-major-mongodb")
+public class MonitorMajorJongoConfig {
 
     private String    clientUri;
     private String    database;
     private Integer   connectionsPerHost;
 
-    @Bean
-    public MongoClient mongoClient() throws Exception {
-        LOGGER.info("init mongoClient start");
-        
+    public MongoClient mongoClient() {
         int connections = Constants.DEFAULT_MONGO_CONNECTION_PER_HOST;
         if(connectionsPerHost != null && connectionsPerHost > 0) {
             connections = connectionsPerHost;
         }
+        
         Builder option = MongoClientOptions.builder().connectionsPerHost(connections);
         MongoClient mongoClient = new MongoClient(new MongoClientURI(clientUri, option));
         return mongoClient;
     }
 
-    @Bean
-    public Jongo jongo(MongoClient mongoClient) {
-        return new Jongo(mongoClient.getDB(database));
+    @Bean("monitorMajorJongo")
+    public Jongo jongo() {
+        return new Jongo(mongoClient().getDB(database));
     }
 
     public String getClientUri() {
