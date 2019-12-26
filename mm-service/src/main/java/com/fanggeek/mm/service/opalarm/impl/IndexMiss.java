3b69c60bd8fe41ac6b5ea2eb7b5df36c4747498e
huangwei <huangwei@fanggeek.com>
2019-12-26 16:00:16 +0800

    提交 三个分析场景

---
+++ mm-service/src/main/java/com/fanggeek/mm/service/opalarm/impl/IndexMiss.java
@@ -0,0 +1,23 @@
+package com.fanggeek.mm.service.opalarm.impl;
+
+import com.fanggeek.mm.dao.model.doc.SlowOpRecordDocument;
+import com.fanggeek.mm.service.opalarm.IAlarm;
+
+/**
+ * <br>索引完全未命中(所有查询字段都没加索引) 
+ *
+ * @author YellowTail
+ * @since 2019-12-25
+ */
+public class IndexMiss implements IAlarm {
+
+    public boolean match(SlowOpRecordDocument doc) {
+        //没有走索引
+        return doc.getKeysExamined() == 0;
+    }
+
+    public String tips() {
+        return "天啦撸！ 没有加索引！";
+    }
+
+}
---
+++ mm-service/src/main/java/com/fanggeek/mm/service/opalarm/impl/IndexPart.java
@@ -0,0 +1,30 @@
+package com.fanggeek.mm.service.opalarm.impl;
+
+import com.fanggeek.mm.dao.model.doc.SlowOpRecordDocument;
+import com.fanggeek.mm.service.opalarm.IAlarm;
+
+/**
+ * <br>索引部分未命中(部分字段没加索引) 
+ *
+ * @author YellowTail
+ * @since 2019-12-25
+ */
+public class IndexPart  implements IAlarm  {
+
+    public boolean match(SlowOpRecordDocument doc) {
+        if (doc.getDocsExamined() > doc.getKeysExamined()) {
+            return true;
+        }
+        
+        if (doc.getPlanSummary().startsWith("COLLSCAN")) {
+            return true;
+        }
+        
+        return false;
+    }
+
+    public String tips() {
+        return "索引有点问题，查询语句没有完全走索引";
+    }
+
+}
